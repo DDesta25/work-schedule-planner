3079d4b219626e3cc344254ca8f13343d7aaad9f
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-05 14:48:24 -0500

    added the jQuary document so the code could run

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -13,6 +13,10 @@ $(function () {
     let id = $(this).parent().attr("id");
     let value = $(this).siblings(".description").val();
     localStorage.setItem(id, value);
+    $(".notifications").addClass("displayBlock");
+    setTimeout(() => {
+      $(".notifications").removeClass("displayBlock");
+    }, 1000);
   });
   //
   // TODO: Add code to apply the past, present, or future class to each time
